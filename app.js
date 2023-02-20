98dd8feedbaaa8d05841c724c3c3621a58382674
Vlad Gerasimchuk <88591743+2Voby@users.noreply.github.com>
2023-02-20 22:08:54 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+// var xhr = new XMLHttpRequest();
+// var fileID = "1pQ_y5Q2EBaCWVDujDgiO-KDL1VZveHKh";
+// let accessToken = "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";
+// xhr.open(
+//   "GET",
+//   "https://www.googleapis.com/drive/v3/files/" + fileID + "?alt=media"
+// );
+// xhr.setRequestHeader("Authorization", "Bearer " + accessToken);
+// xhr.onreadystatechange = function () {
+//   if (xhr.readyState === 4) {
+//     if (xhr.status === 200) {
+//       var json = JSON.parse(xhr.responseText);
+//       console.log(json);
+//     } else {
+//       console.log("Error: " + xhr.status);
+//     }
+//   }
+// };
+
+// var accessToken = "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";
+// var xhr = new XMLHttpRequest();
+// xhr.open(
+//   "GET",
+//   "https://www.googleapis.com/drive/v3/files?access_token=" + accessToken
+// );
+// xhr.onreadystatechange = function () {
+//   if (xhr.readyState === 4) {
+//     if (xhr.status === 200) {
+//       var json = JSON.parse(xhr.responseText);
+//       console.log(json);
+//     } else {
+//       console.log("Error: " + xhr.status);
+//     }
+//   }
+// };
+// xhr.send();
+var fileId = "1pX4o6LEVebUWApXGV4FQ1JlP0lMWvxjR";
+var apiKey = "AIzaSyDFWdO7gAJOwGtS2VEIvmII1uoBwW2R_Eg";
+var url =
+  "https://www.googleapis.com/drive/v3/files/" +
+  fileId +
+  "?key=" +
+  apiKey +
+  "&alt=media";
+
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.error(error));
